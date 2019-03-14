fdff5b5c3bb5c21a83c6d59972da9a1901866405
claytonschneider <claykschneider@gmail.com>
2019-03-13 19:17:35 -0700

    1 file modified

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div>
         <Hello name={this.state.name} />
         <p>
-          Hello dang disdfdfsngle
+          Hello dangsngle
         </p>
       </div>
     );
